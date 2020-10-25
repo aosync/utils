b46162324d0ce852c9b44824595744489d35eda8
Alejandro Sior <aho@sior.be>
2020-10-25 17:53:39 +0100

    updated mkdir

--- commands/mkdir/mkdir.go
+++ commands/mkdir/mkdir.go
@@ -10,7 +10,7 @@ import (
 
 var opts []string
 var p bool = false
-var mode int = 0755
+var mode uint = 0755
 
 func Mkdir(opts []string) int {
 	rs := []xbd.OptRule{xbd.OptRule{'p', false}, xbd.OptRule{'m', true}}
@@ -20,12 +20,12 @@ func Mkdir(opts []string) int {
 	b, _ := xbd.Encountered(e, 'p')
 	p = b
 	if b, v := xbd.Encountered(e, 'm'); b {
-		d, err := strconv.Atoi(v)
+		d, err := strconv.ParseUint(v, 8, 32)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "mkdir: invalid mode\n")
 			return 1
 		}
-		mode = d
+		mode = uint(d)
 	}
 
 	fmt.Println(mode)
